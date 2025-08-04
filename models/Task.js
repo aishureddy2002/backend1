e6ea0058aa57cd57bdc7a61f923f68942f01056d
aishureddy2002 <aishuredd.2002@gmail.com>
2025-08-04 17:20:53 -0400

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -4,5 +4,4 @@ const TaskSchema = new mongoose.Schema({
   text: { type: String, required: true }
 });
 
-const Task = mongoose.model("Task", TaskSchema);
-export default Task;
+export default mongoose.model("Task", TaskSchema);
